d06de73d76c874e95292ac448b9eec805ab7d3ea
Bug <jungmokang99@gmail.com>
2023-05-22 21:12:46 -0400

    Added tesla account page and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import {
@@ -10,33 +10,88 @@ import {
 import Menu from "./components/Menu";
 import HeaderBlock from "./components/HeaderBlock";
 import Login from "./components/Login";
-import { useSelector } from "react-redux";
-import { selectUser } from "./features/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
+import Signup from "./components/Signup";
+import TeslaAccount from "./components/TeslaAccount";
+import { auth } from "./firebase";
 
 function App() {
-  const user = useSelector(selectUser)
+  const user = useSelector(selectUser);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //user is signed in
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+          })
+        );
+      } else {
+        //user is signed out
+        dispatch(logout());
+      }
+    });
+  }, [dispatch]);
 
   return (
     <div className="app">
       <Router>
         {/* <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} /> */}
         {/* <HeaderBlock /> */}
-        {isMenuOpen && <Menu />}
+        {/* {isMenuOpen && <Menu />} */}
         <Routes>
           <Route
             path="/"
             element={
               <>
+                {isMenuOpen && <Menu />}
                 <HeaderBlock />
                 <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
               </>
             }
           ></Route>
-          <Route path="/login" element={<Login />} ></Route>
+
+          <Route
+            path="/login"
+            element={
+              user ? (
+                <Navigate to="/teslaaccount" />
+              ) : (
+                <Route path="/login" element={<Login />}></Route>
+              )
+            }
+          ></Route>
+
           <Route path="/signup" element={<Signup />}></Route>
+
+          <Route
+            path="/teslaaccount"
+            element={
+              !user ? (
+                <Navigate to="/login" />
+              ) : (
+                <Route
+                  path="/teslaaccount"
+                  element={
+                    <>
+                      <TeslaAccount
+                        isMenuOpen={isMenuOpen}
+                        setIsMenuOpen={setIsMenuOpen}
+                      />
+                      {isMenuOpen && <Menu />}
+                    </>
+                  }
+                ></Route>
+              )
+            }
+          ></Route>
         </Routes>
-        {/* {user ? <Navigate to='/teslaaccount'/> : <Login/>} */}
       </Router>
     </div>
   );
---
+++ src/components/Signup.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import "./Signup.css";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import LanguageOutlinedIcon from "@material-ui/icons/LanguageOutlined";
+import ButtonPrimary from "./ButtonPrimary";
+import ButtonSecondary from "./ButtonSecondary";
+import { auth } from "../firebase";
+import { login } from "../features/userSlice";
+
+function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const signUp = (e) => {
+    e.preventDefault();
+
+    if (!fName) {
+      return alert("Please enter a first name!");
+    }
+    if (!lName) {
+      return alert("Please enter a last name!");
+    }
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userAuth) => {
+        userAuth.user
+          .updateProfile({
+            displayName: fName,
+          })
+          .then(() => {
+            dispatch(
+              login({
+                email: userAuth.user.email,
+                uid: userAuth.user.uid,
+                displayName: fName,
+              })
+            );
+            navigate.push("/teslaaccount");
+          });
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  return (
+    <div className="signup">
+      <div className="signup__header">
+        <div className="signup__logo">
+          <Link>
+            <img
+              src="https://assets.website-files.com/5e8fceb1c9af5c3915ec97a0/5ec2f037975ed372da9f6286_Tesla-Logo-PNG-HD.png"
+              alt=""
+            />
+          </Link>
+        </div>
+        <div className="signup__language">
+          <LanguageOutlinedIcon /> <span>en-US</span>
+        </div>
+      </div>
+      <div className="signup__info">
+        <h1>Create Account</h1>
+        <form className="signup__form">
+          <label htmlFor="fName">First Name</label>
+          <input
+            type="text"
+            id="fName"
+            value={fName}
+            onChange={(event) => setFName(event.target.value)}
+          />
+          <label htmlFor="lName">Last Name</label>
+          <input
+            type="text"
+            id="lName"
+            value={lName}
+            onChange={(event) => setLName(event.target.value)}
+          />
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <ButtonPrimary name="create account" type="submit" onClick={signUp} />
+        </form>
+        <div className="signup__divider">
+          <hr /> <span>OR</span> <hr />
+        </div>
+        <Link to="/login">
+          <ButtonSecondary name="sign in" />
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
